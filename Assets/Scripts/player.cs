61cba7acf9c5e09001d1f1c8e3e69313d3d8afa4
Arjun <arjuunz@gmail.com>
2014-10-19 16:43:53 +0530

    Initial

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player : MonoBehaviour {
+
+	public float speed;
+	public GUIText countText, winText;
+	private int count;
+
+	void Start() {
+		count = 0;
+		gameCount ();
+		winText.text = "";
+	}
+
+	void FixedUpdate() {
+		//float moveHorizontal = Input.GetAxis ("Horizontal");
+		//float moveVertical = Input.GetAxis ("Vertical");
+
+		float moveHorizontal = Input.acceleration.x;
+		float moveVertical = Input.acceleration.y;
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if(other.gameObject.tag == "PickUp") {
+			other.gameObject.SetActive (false);
+			count++;
+			gameCount();
+		}
+	}
+
+	void gameCount() {
+		countText.text = "Score : " + count.ToString ();
+		if (count >= 4) {
+			winText.text="You win!";
+		}
+	}
+}
---
+++ Assets/Scripts/rotater.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotater : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(15,30,25) * Time.deltaTime);
+	}
+}
